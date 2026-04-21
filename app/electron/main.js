ada3185949e24570e5929d31e4fd90a37c5b7328
thenoname-gurl <plurryt@gmail.com>
2026-04-21 17:48:53 +0000

    Add our beloved electron

---
+++ app/electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const appUrl = process.env.APP_URL || 'https://ecli.app';
+  mainWindow.loadURL(appUrl);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
